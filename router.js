6176347a7cc631a2fa8a512ac9220a9eacaf3666
Dhrunit <jimmyprajapati99@gmail.com>
2020-11-23 11:10:24 +0530

    Added a route for editing a post behaviour

--- router.js
+++ router.js
@@ -30,4 +30,8 @@ router.post(
 );
 router.get('/post/:id', postController.viewSingle);
 
+router.get('/post/:id/edit', postController.viewEditScreen);
+
+router.post('/post/:id/edit', postController.edit);
+
 module.exports = router;
