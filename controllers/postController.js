000eadc560a781b2ec373e08515f7033641a4f91
Dhrunit <jimmyprajapati99@gmail.com>
2020-11-23 10:47:59 +0530

    Added logic to check whether the visitor is the owner of the post

--- controllers/postController.js
+++ controllers/postController.js
@@ -17,7 +17,7 @@ exports.create = (req, res) => {
 
 exports.viewSingle = async function (req, res) {
 	try {
-		let post = await Post.findSingleById(req.params.id);
+		let post = await Post.findSingleById(req.params.id, req.visitorId);
 		console.log(post);
 		res.render('single-post-screen', { post: post });
 	} catch (error) {
