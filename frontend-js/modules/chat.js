009381da6b7e42293c283ed3ba626e0d3339b48a
Dhrunit <jimmyprajapati99@gmail.com>
2020-12-22 13:59:03 +0530

    Frontend logic to leverage socket.io

--- frontend-js/modules/chat.js
+++ frontend-js/modules/chat.js
@@ -4,12 +4,18 @@ export default class Chat {
 		this.chatWrapper = document.querySelector('#chat-wrapper')
 		this.openIcon = document.querySelector('.header-chat-icon')
 		this.injectHTML()
+		this.chatField = document.querySelector('#chatField')
+		this.chatForm = document.querySelector('#chatForm')
 		this.closeIcon = document.querySelector('.chat-title-bar-close')
 		this.events()
 	}
 
 	// Events
 	events() {
+		this.chatForm.addEventListener('submit', (e) => {
+			e.preventDefault()
+			this.sendMessageToServer()
+		})
 		this.openIcon.addEventListener('click', () => {
 			this.showChat()
 		})
@@ -18,6 +24,14 @@ export default class Chat {
 		})
 	}
 
+	// Methods
+	sendMessageToServer() {
+		this.socket.emit('chatMessageFromBrowser', {
+			message: this.chatField.value,
+		})
+		this.chatField.value = ''
+		this.chatField.focus()
+	}
 	hideChat() {
 		this.chatWrapper.classList.remove('chat--visible')
 	}
@@ -31,10 +45,12 @@ export default class Chat {
 	}
 
 	openConnection() {
-		alert('yes')
+		this.socket = io()
+		this.socket.on('chatMessageFromServer', (data) => {
+			alert(data.message)
+		})
 	}
 
-	// Methods
 	injectHTML() {
 		this.chatWrapper.innerHTML = `
         <div class="chat-title-bar">Chat <span class="chat-title-bar-close"><i class="fas fa-times-circle"></i></span></div>
